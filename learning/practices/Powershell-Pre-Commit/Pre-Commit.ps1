9f4e23d28349b3b64c1ad6f223a17acbe24ba0a9
jiayang <jiayang@yuntech.edu.tw>
2021-01-20 11:19:52 +0800

    <feature>: (新增pre-commit.ps1) </feature>
    進行git email驗證
    變更索引若含有.cs file變更，尋找.net project
    並依序建置與測試

---
+++ learning/practices/Powershell-Pre-Commit/Pre-Commit.ps1
@@ -0,0 +1,27 @@
+param (
+        [string]$email
+    )
+Write-Information "begin load powershell script" -InformationAction Continue
+. $PSScriptRoot/Git/Confirm-GitEmail.ps1
+. $PSScriptRoot/Git/Get-CommittedFiles.ps1
+. $PSScriptRoot/Git/Get-ChangedFileRootDirectory
+. $PSScriptRoot/NetProject/Invoke-NetProject
+if((Confirm-GitEmail -Email $email) -eq 1)
+{
+    exit 1
+}
+Write-Information "get committed files" -InformationAction Continue
+$repositoryDirectory =Split-Path -Parent $PSScriptRoot
+$changedFile=Get-CommittedFiles
+$changedFileInfos=Get-ChangedFileRootDirectory -fileNames $changedFile -RootDirectory $repositoryDirectory | Where-Object {$_.Name -like "*.cs"}
+$executeResult=@()
+foreach($file in $changedFileInfos)
+{
+    Write-Information "execute net project" -InformationAction Continue
+    $result=Invoke-NetProject -Path $repositoryDirectory -File $changedFile
+    $executeResult+=$result
+}
+if($executeResult -contains 1)
+{
+    exit 1
+}
