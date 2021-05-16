f53ba8b09b70d4a87533e5d38626926f321334a5
jiayang <jiayang@yuntech.edu.tw>
2021-05-16 20:57:21 +0800

    <feature>:(ClassifiedAd新增建構子)</feature>
    新增建構子，並提供參數設定id
    檢查id是否有提供值，若為預設值則拋出例外

--- devops/2-code/practices/maketplace/src/services/marketplace.domain/ClassifiedAd.cs
+++ devops/2-code/practices/maketplace/src/services/marketplace.domain/ClassifiedAd.cs
@@ -6,11 +6,17 @@ namespace marketplace.domain
     {
         public Guid Id { get; private set; }
         public Guid _ownerId;
-
         public string _title;
-
         public string _text;
         public string _price;
 
+        public ClassifiedAd(Guid id)
+        {
+            if(id==default)
+            {
+                throw new ArgumentException("Identity must be specified", nameof(Id));
+            }
+            Id = id;
+        }
     }
 }
