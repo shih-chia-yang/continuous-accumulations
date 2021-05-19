753785ca882a3344eec78c8223fa5bec4a185e23
jiayang <jiayang@yuntech.edu.tw>
2021-05-19 22:09:40 +0800

    <feature>:(Add ClassifiedAdText)</feature>
    將Text提取出實作成ClassifiedAdText類別，並將Text的型別改
    為ClassifiedAdText

--- devops/2-code/practices/maketplace/src/services/marketplace.domain/entities/ClassifiedAd.cs
+++ devops/2-code/practices/maketplace/src/services/marketplace.domain/entities/ClassifiedAd.cs
@@ -3,13 +3,22 @@ using marketplace.domain.entities;
 
 namespace marketplace.domain
 {
+    public enum ClassifiedState
+    {
+        PendingReview,
+        Active,
+        Inactive,
+        MarkedAsSold
+    }
     public class ClassifiedAd
     {
         public Guid Id { get; private set; }
-        private UserId _ownerId;
-        private ClassifiedAdTitle _title;
-        private string _text;
-        private Price _price;
+        public UserId OwnerId { get;}
+        public ClassifiedAdTitle Title { get; private set; }
+        public ClassifiedAdText Text { get; private set; }
+        public Price Price { get; private set; }
+
+        public ClassifiedState State { get; private set;}
         public ClassifiedAd(Guid id,UserId ownerId)
         {
             if(id==default)
@@ -17,13 +26,19 @@ namespace marketplace.domain
                 throw new ArgumentException("Identity must be specified", nameof(Id));
             }
             Id = id;
-            _ownerId=ownerId;
+            OwnerId=ownerId;
+            State = ClassifiedState.Inactive;
         }
 
-        public void SetTitle(ClassifiedAdTitle title) => _title = title;
+        public void SetTitle(ClassifiedAdTitle title) => Title = title;
 
-        public void UpdateText(string text) => _text = text;
+        public void UpdateText(ClassifiedAdText text) => Text = text;
 
-        public void UpdatePrice(Price price) => _price = price;
+        public void UpdatePrice(Price price) => Price = price;
+
+        public void RequestToPublish()
+        {
+            this.State = ClassifiedState.PendingReview;
+        }
     }
 }
