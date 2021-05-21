035995b542fd7f47aaf36239cbdc1b831f179f76
jiayang <jiayang@yuntech.edu.tw>
2021-05-21 15:23:25 +0800

    <feature>:(add ClassifiedAdCreatedCommand in controller)</feature>
    在controller中的建構子宣告參數，並使用di接收註冊物件
    在endpoint中將從http request接收到的contract強型別類別
    傳入application service中執行

--- devops/2-code/practices/maketplace/src/services/marketplace.api/Controllers/ClassifiedAdController.cs
+++ devops/2-code/practices/maketplace/src/services/marketplace.api/Controllers/ClassifiedAdController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using marketplace.api.Applications.Command;
 using marketplace.api.Applications.Contracts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,14 +13,17 @@ namespace marketplace.api.Controllers
     [ApiController]
     public class ClassifiedAdController : ControllerBase
     {
-        public ClassifiedAdController()
+        private readonly ClassifiedAdCreatedCommand _created;
+
+        public ClassifiedAdController(ClassifiedAdCreatedCommand created)
         {
-            
+            _created = created;
         }
 
         [HttpPost]
         public async Task<IActionResult> Add (ClassifiedAds.V1.Create request)
         {
+            _created.Handle(request);
             return Ok();
         }
     }
